cc168839b21608f5de9177771a45199cc30646b9
chiragpande-sys <readthefabulousmanual@gmail.com>
2024-08-19 18:15:27 +0530

    Deploy website - based on 63621db09f123123547af6555bb9e34e71caaa95

---
+++ assets/js/62b081b1.a4f3abff.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreadthefabmanual=self.webpackChunkreadthefabmanual||[]).push([[6679],{5185:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/readthefabulousmanual/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.3b6a4ace.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreadthefabmanual=self.webpackChunkreadthefabmanual||[]).push([[1903],{1912:a=>{a.exports=JSON.parse('{"blogBasePath":"/readthefabulousmanual/blog","blogTitle":"Blog","authorsListPath":"/readthefabulousmanual/blog/authors"}')}}]);
